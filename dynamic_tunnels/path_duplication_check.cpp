5eb2a17ff0b32fb534dcad8149c35fa90a7b3b10
Unknown <kristron6@gmail.com>
2017-12-26 02:21:00 +0100

    ANOTHET FUGGIN FIX, FUG A HELL

--- dynamic_tunnels/path_duplication_check.cpp
+++ dynamic_tunnels/path_duplication_check.cpp
@@ -84,99 +84,71 @@ int find_extreme_point(shared_ptr<Path> tunnel, double* center, int start_index,
 //Uses Z_interval parameters for not counting parts around beginning or ending. If used, delegation into intervals will start at first unremoved node and end at last node before the removed ones
 //at the end.
 bool create_N_representation(shared_ptr<Path> tunnel){
-	int endpoint_index = tunnel->get_endpoint_index();
-	int start_index = tunnel->get_beginning_index();
+	int cutted_part_end_index = tunnel->get_endpoint_index();
+	int cutted_part_start_index = tunnel->get_beginning_index();
 	std::map<int, std::shared_ptr<vertex>> tunnel_map = tunnel->get_vertices();
-	double* tunnel_end_coordinate = tunnel_map[endpoint_index]->get_location_coordinates();
-	double* tunnel_root_coordinate = tunnel_map[start_index]->get_location_coordinates();
+	double* tunnel_end_coordinate = tunnel->get_endpoint_node()->get_location_coordinates();
+	double* tunnel_root_coordinate = tunnel->get_beginning_node()->get_location_coordinates();
 	
     //these take into account from where the representation is computed, ie where the the tunnel starts after getting rid of Z_start interval
 	int start_offset = 0;
 	int end_offset = 0;
 
-	std::shared_ptr<vertex> cur = tunnel->get_node_pointer(start_index);
-	int counter = 0;
+	std::shared_ptr<vertex> cur = tunnel->get_beginning_node();
 	bool are_endings_cut = true;
 
 	
 
 	//gets rid of nodes in the z radii
 	double tunnel_offset = DBL_MAX;
-	std::map<int, shared_ptr<vertex>>::iterator iterator;
-  	for(iterator = tunnel_map.begin(); iterator != tunnel_map.end(); iterator++){
-  		double cur_distance_from_root = compute_metric_eucleidean(iterator->second->get_location_coordinates(), tunnel_root_coordinate);
-  		if(cur_distance_from_root<= Z_start_radius && (iterator->first > start_index)){
-  			start_index = iterator->first;
+	int counter = 0;
+
+  	while(true){
+  		double cur_distance_from_root = compute_metric_eucleidean(cur->get_location_coordinates(), tunnel_root_coordinate);
+  		if(cur_distance_from_root<= Z_start_radius) {
+  			cutted_part_start_index = cur->get_index();
   			
   		} else if(cur_distance_from_root > Z_start_radius){
   			if(cur_distance_from_root < tunnel_offset){
   				tunnel_offset = cur_distance_from_root;
   			}
   		}
-  		if(check_if_in_sphere(iterator->second->get_location_coordinates(), Z_end_radius, tunnel_end_coordinate) && (iterator->first < endpoint_index)){
-  			endpoint_index = iterator->first;
-  		}
-  		if(start_index >= endpoint_index){
-  			are_endings_cut = false;
-  			start_index = tunnel->get_beginning_index();
-  			endpoint_index = tunnel->get_endpoint_index();
-  			tunnel_offset = 0;
+  		if(check_if_in_sphere(cur->get_location_coordinates(), Z_end_radius, tunnel_end_coordinate)){
+  			cutted_part_end_index = cur->get_index();
   			break;
-  		} 	
+  		}
+  		if(tunnel->is_node_endpoint(cur)){
+  			break;
+  		}
+  		cur = cur->get_child_pointer().lock();
+  			
 	}
-	//we need to use the index next to the last one colliding with z_interval sphere
-	start_index = tunnel_map[start_index]->get_child_index();
-	endpoint_index = tunnel_map[endpoint_index]->get_parent_index();
 
-		/*
-	while(true){
-		if(counter == tunnel->get_size() - 1){
-			start_index = tunnel->get_beginning_index();
-			are_endings_cut = false;
-			break;
-		}
-		if(check_if_in_sphere(cur->get_location_coordinates(), Z_end_radius, tunnel_root_coordinate)){
-			start_offset++;
-		} else {
-			start_index = cur->get_index();
-			break;
-		}
-		cur = tunnel->get_node_pointer(tunnel->get_child_index(cur->get_index()));
-		//std::cout << "N_rep loop1 " << counter << std::endl; 
-		counter++;
-	}
-	cur = tunnel->get_node_pointer(endpoint_index);
+	//we need to use the index next to the last one colliding with z_interval sphere; 
+	cutted_part_start_index = tunnel->get_node_pointer(cutted_part_start_index)->get_child_pointer().lock()->get_index();
+	cutted_part_end_index = tunnel->get_node_pointer(cutted_part_end_index)->get_parent_pointer().lock()->get_index();
 
-	counter = 0;
-	if(are_endings_cut){
-		while(true){
-			if(check_if_in_sphere(cur->get_location_coordinates(), Z_end_radius, tunnel_end_coordinate)){
-				end_offset++;
-			} else {
-				endpoint_index = cur->get_index();
-				break;
-			}
-			cur = tunnel->get_node_pointer(tunnel->get_parent_index(cur->get_index()));
-			//std::cout << "N_rep loop2 " << counter << std::endl; 
-			counter++;
-		}
-	} else {
-		endpoint_index = tunnel->get_endpoint_index();
-	}
-	*/
+	if(cutted_part_start_index >= cutted_part_end_index){
+		are_endings_cut = false;
+		cutted_part_start_index = tunnel->get_beginning_index();
+		cutted_part_end_index = tunnel->get_endpoint_index();
+		tunnel_offset = 0;
+	} 
+	
+
 	//---gets rid of nodes in the z radii
 
 	//locates node lying farthest from the tunnel centre, this is used as basis for the size of the intervals
-	int extreme_point = find_extreme_point(tunnel, tunnel_root_coordinate, start_index, endpoint_index);
+	int extreme_point = find_extreme_point(tunnel, tunnel_root_coordinate, cutted_part_start_index, cutted_part_end_index);
 	double extreme_point_distance = compute_metric_eucleidean(tunnel_map[extreme_point]->get_location_coordinates(), tunnel_root_coordinate) - tunnel_offset;
 	//coordinate of the first node used for interval computation
-	double* tunnel_beginning_coordinate = tunnel_map[start_index]->get_location_coordinates();
+	double* tunnel_beginning_coordinate = tunnel_map[cutted_part_start_index]->get_location_coordinates();
 	//we must subtract the distance from the tunnel's first to node to first node after getting rid of nodes within z_interval, so that we can start at zero, which is required
 	//for dividing into intervals correctly
 	//cout << "entering test" << endl;
 	//print_vector(tunnel_map[tunnel->get_beginning_index()]->get_location_coordinates());
 
-	int index = start_index;
+	int index = cutted_part_start_index;
 	std::vector<int> N_counts(N, 0);
 	//just init zero values
 	std::vector<double*> N_representation;
@@ -200,18 +172,20 @@ bool create_N_representation(shared_ptr<Path> tunnel){
 		double cur_distance = compute_metric_eucleidean(tunnel_map[index]->get_location_coordinates(), tunnel_map[tunnel->get_beginning_index()]->get_location_coordinates()) - tunnel_offset;
 		//compute distance of current node from the tunnel beginning node
 		if(DEBUG_ENABLED){
+			cout << "counter " << counter << endl;
 			cout << "debug" <<endl;
 			cout << "tunnel_offset " << tunnel_offset << endl;
 			cout << "cur distance " <<  cur_distance << endl;
 			cout << "extreme point distance " << extreme_point_distance << endl;
-			cout << "last node" << endpoint_index << endl;
+			cout << "first node " << cutted_part_start_index << endl;
+			cout << "last node" << cutted_part_end_index << endl;
 			cout << "endpoit" << tunnel->get_endpoint_index() << endl;
 		}
 		if(TESTING_ENABLED){
 			if(cur_distance > extreme_point_distance){
 				cout << "TEST ERROR: node in tunnel is farther from center than extreme point in duplication check!" << endl;
 				cout << "extreme point index " << extreme_point<< endl;
-				cout << "endpoint_index " << endpoint_index << endl;
+				cout << "endpoint_index " << cutted_part_end_index << endl;
 				cout << "current index " << index << endl;
 				exit(0);
 			}
@@ -244,7 +218,7 @@ bool create_N_representation(shared_ptr<Path> tunnel){
 
 
 		
-		if(index == endpoint_index){
+		if(index == cutted_part_end_index){
 			break;
 		}
 		index = tunnel->get_child_index(index);
@@ -253,7 +227,7 @@ bool create_N_representation(shared_ptr<Path> tunnel){
 		counter++; 
 	}
 
-	index = start_index;
+	index = cutted_part_start_index;
 
 	counter = 0;
 	index = 0;
@@ -302,14 +276,24 @@ void decide_N(std::vector<shared_ptr<Path>>& paths, shared_ptr<Path> path){
 }
 
 //obvious
-bool recompute_N_representation(shared_ptr<Path> path){
-	path->reset_N_representation();
-	bool ret =  create_N_representation(path);
-	//if(!ret){
-	//	cout << "FAILLLL" << endl;
-	//}
-	return ret;
+bool recompute_N_representation(std::vector<shared_ptr<Path>> existing_paths, shared_ptr<Path> path, bool try_without_changing_N){
+	bool N_changed = false;
+	bool successful = false;
+
+	if(try_without_changing_N){
+		path->reset_N_representation();
+		successful = create_N_representation(path);
+	} 
 	
+	while(!successful){
+		N_changed = true;
+		INTERVAL_RATIO_CONSTANT *= 1.1;
+		decide_N(existing_paths, path);
+		path->reset_N_representation();
+		successful = create_N_representation(path);
+	}
+
+	return N_changed;	
 }
 
 
@@ -317,19 +301,31 @@ bool recompute_N_representation(shared_ptr<Path> path){
 //we get smaller number of intervals.
 void N_representation(std::vector<shared_ptr<Path>> existing_paths, shared_ptr<Path> checked_path){
 	int counter = 0;
-	bool was_succesful;
+	bool was_successful = true;
 	bool recompute = false;
-	was_succesful = create_N_representation(checked_path);
-	while(!was_succesful){
-		recompute = true;
-		INTERVAL_RATIO_CONSTANT *= 1.2;
-		decide_N(existing_paths, checked_path);			
-		was_succesful = recompute_N_representation(checked_path);
-	} 
 
+	//find working N for the new path
+	if(!create_N_representation(checked_path)){
+		recompute = true;
+		recompute_N_representation(existing_paths, checked_path, false);
+	}
+
+	bool recompute_checked_path = false;
 	if(recompute && existing_paths.size() > 0){
-		for(std::vector<shared_ptr<Path>>::iterator iterator = existing_paths.begin(); iterator != existing_paths.end(); iterator++){
-			recompute_N_representation(*iterator);
+		for(int i = 0; i < existing_paths.size(); i++){
+			if(recompute_checked_path){
+				recompute_N_representation(existing_paths, checked_path, false);
+				recompute_checked_path = false;
+			}
+			//N changed, must start from beginning to keep sizes consistent
+			if(recompute_N_representation(existing_paths, existing_paths[i], true))
+			{
+//				cout << "RESTART!" << endl;
+				i = -1;
+				recompute_checked_path = true;
+				continue;
+			}
+//			cout << "new size " << existing_paths[i]->N_representation.size() << endl;
 		}
 	}
 }
@@ -338,17 +334,17 @@ int is_tunnel_duplicated(shared_ptr<Path> checked_path, std::vector<shared_ptr<P
 	if(existing_paths.size() == 0){
 		decide_N(existing_paths, checked_path);
 		N_representation(existing_paths, checked_path);
-		cout << "N " << N << std::endl;
-		cout << "first " << checked_path->N_representation.size() << endl;
+//		cout << "N " << N << std::endl;
+//		cout << "first " << checked_path->N_representation.size() << endl;
 		return -1;
 	}
 
 	N_representation(existing_paths, checked_path);
 
-	cout << "N " << N << std::endl;
-	cout << "checked N repr " << checked_path->N_representation.size() << endl;
+//	cout << "N " << N << std::endl;
+//	cout << "checked N repr " << checked_path->N_representation.size() << endl;
 	for(int i = 0; i < existing_paths.size(); i++){
-		cout << "checked against N repr " << existing_paths[i]->N_representation.size() << endl;                         
+//		cout << "checked against N repr " << existing_paths[i]->N_representation.size() << endl;                         
 		double distance = compute_intertunnel_distance_by_N_representations(existing_paths[i], checked_path);
 		//std::cout << "distance " << distance << std::endl;
 		//std::cout << "final N " << N << std::endl;
